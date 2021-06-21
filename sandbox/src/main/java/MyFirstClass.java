ea6e9c471c026caf8a0e183094a7f5a424f3383a
Shrekt1 <85940632+Shrekt1@users.noreply.github.com>
2021-06-21 12:24:23 +0300

    Добавлен конструктор класса, функция вычисления перенесена внутрь класса

--- sandbox/src/main/java/MyFirstClass.java
+++ sandbox/src/main/java/MyFirstClass.java
@@ -2,16 +2,13 @@ public class MyFirstClass {
 
     public static void main(String[] args) {
 
-        Point p1 = new Point();
-        p1.x = 4;
-        p1.y = 3;
-        Point p2 = new Point();
-        p2.x = 0;
-        p2.y = 0;
-        System.out.println("Расстояние между точками (" + p1.x + ", " + p1.y + ") и (" + p2.x + ", " + p2.y + ") = " + distance(p1,p2));
+        Point p1 = new Point(4,3);
+        Point p2 = new Point(0,0);
+        System.out.println("Расстояние между точками (" + p1.x + ", " + p1.y + ") и (" + p2.x + ", " + p2.y + ") = " + p1.distance(p2));
     }
-    public static double distance(Point p1, Point p2){
-        return Math.sqrt(Math.pow((p1.x - p2.x),2) + Math.pow((p1.y - p2.y),2));
+
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p2.y), 2));
     }
 
 }
